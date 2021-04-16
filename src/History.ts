f044c78917b8b5145578251d8e2700ca66d412a5
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-04-16 18:52:15 +0200

    :art: prettier

--- src/History.ts
+++ src/History.ts
@@ -1,50 +1,50 @@
 export interface History {
-  noofslots:           number;
-  day_size:            string;
-  week_size:           string;
-  month_size:          string;
-  total_size:          string;
+  noofslots: number;
+  day_size: string;
+  week_size: string;
+  month_size: string;
+  total_size: string;
   last_history_update: number;
-  slots:               Slot[];
+  slots: Slot[];
 }
 
 export interface Slot {
-  action_line:   string;
-  series:        string;
-  show_details:  string;
-  script_log:    string;
-  meta:          null;
-  fail_message:  string;
-  loaded:        boolean;
-  id:            number;
-  size:          string;
-  category:      string;
-  pp:            string;
-  retry:         number;
-  completeness:  number;
-  script:        string;
-  nzb_name:      string;
+  action_line: string;
+  series: string;
+  show_details: string;
+  script_log: string;
+  meta: null;
+  fail_message: string;
+  loaded: boolean;
+  id: number;
+  size: string;
+  category: string;
+  pp: string;
+  retry: number;
+  completeness: number;
+  script: string;
+  nzb_name: string;
   download_time: number;
-  storage:       string;
-  has_rating:    boolean;
-  status:        string;
-  script_line:   string;
-  completed:     number;
-  nzo_id:        string;
-  downloaded:    number;
-  report:        string;
-  password:      string;
-  path:          string;
+  storage: string;
+  has_rating: boolean;
+  status: string;
+  script_line: string;
+  completed: number;
+  nzo_id: string;
+  downloaded: number;
+  report: string;
+  password: string;
+  path: string;
   postproc_time: number;
-  name:          string;
-  url:           string;
-  md5sum:        string;
-  bytes:         number;
-  url_info:      string;
-  stage_log:     StageLog[];
+  name: string;
+  url: string;
+  md5sum: string;
+  bytes: number;
+  url_info: string;
+  stage_log: StageLog[];
 }
 
 export interface StageLog {
-  name:    string;
+  name: string;
   actions: string[];
 }
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,8 @@ import { URL } from "url";
 import { Queue } from "./Queue";
 import { History } from "./History";
 
-
 export class SABnzbd {
   private readonly url: URL;
-
 
   /**
    * Base SABnzbd class
@@ -20,8 +18,12 @@ export class SABnzbd {
    * new SABnzbd("127.0.0.1", "8080", "6e961fd677a74326a07f1df4c06e3f38")
    * ```
    */
-  constructor(private readonly host: string, private readonly port: string, private readonly apiKey: string) {
-    this.url = new URL(`http://${ host }:${ port }/sabnzbd/api`);
+  constructor(
+    private readonly host: string,
+    private readonly port: string,
+    private readonly apiKey: string
+  ) {
+    this.url = new URL(`http://${host}:${port}/sabnzbd/api`);
     this.url.searchParams.set("apikey", this.apiKey);
     this.url.searchParams.set("output", "json");
   }
@@ -30,8 +32,8 @@ export class SABnzbd {
    * Get version of running SABnzbd
    */
   public async version(): Promise<string> {
-    const params = new URLSearchParams({ "mode": "version" });
-    const { version } = await this.request(params) as { version: string };
+    const params = new URLSearchParams({ mode: "version" });
+    const { version } = (await this.request(params)) as { version: string };
     return version;
   }
 
@@ -40,14 +42,20 @@ export class SABnzbd {
    * @param url URL-encoded version of the link to the NZB to be fetched
    * @param params Input parameter
    */
-  public async addURL(url: string, params: AddURLParams = {}): Promise<string[]> {
-    const searchParams = new URLSearchParams({ "mode": "addurl", name: url });
+  public async addURL(
+    url: string,
+    params: AddURLParams = {}
+  ): Promise<string[]> {
+    const searchParams = new URLSearchParams({ mode: "addurl", name: url });
     for (let paramsKey in params) {
       if (params.hasOwnProperty(paramsKey)) {
         searchParams.set(paramsKey, params[paramsKey]);
       }
     }
-    const { nzo_ids } = await this.request(searchParams) as { status: boolean, nzo_ids: string[] };
+    const { nzo_ids } = (await this.request(searchParams)) as {
+      status: boolean;
+      nzo_ids: string[];
+    };
     return nzo_ids;
   }
 
@@ -56,11 +64,11 @@ export class SABnzbd {
    * @param ids Filter jobs by ids
    */
   public async queue(ids?: string[]): Promise<Queue> {
-    const searchParams = new URLSearchParams({ "mode": "queue" });
+    const searchParams = new URLSearchParams({ mode: "queue" });
     if (ids) {
       searchParams.set("nzo_ids", ids.join(","));
     }
-    const { queue } = await this.request(searchParams) as { queue: Queue };
+    const { queue } = (await this.request(searchParams)) as { queue: Queue };
     return queue;
   }
 
@@ -69,13 +77,16 @@ export class SABnzbd {
    * @param url URL-encoded version of the link to the NZB to be fetched
    * @param params Input parameter
    */
-  public async addFileAndWaitTillFinish(url: string, params: AddURLParams = {}): Promise<string[]> {
+  public async addFileAndWaitTillFinish(
+    url: string,
+    params: AddURLParams = {}
+  ): Promise<string[]> {
     const ids = await this.addURL(url, params);
     while (true) {
       if (await this.isDownloadFinished(ids)) {
         return ids;
       }
-      await new Promise(resolve => setTimeout(resolve, 1000));
+      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
     // let isDownloading = false;
     // while (true) {
@@ -113,9 +124,8 @@ export class SABnzbd {
     //  Check the history
     const history = await this.history(ids);
     for (let slot of history.slots) {
-      if (slot.status === "Failed") throw  new Error("download failed");
+      if (slot.status === "Failed") throw new Error("download failed");
       if (slot.status !== "Completed") return false;
-
     }
     return true;
   }
@@ -125,40 +135,41 @@ export class SABnzbd {
    * @param ids Filter jobs by ids
    */
   public async history(ids: string[] = []): Promise<History> {
-    const searchParams = new URLSearchParams({ "mode": "history" });
+    const searchParams = new URLSearchParams({ mode: "history" });
     if (ids.length > 0) {
       searchParams.set("nzo_ids", ids.join(","));
     }
-    const { history } = await this.request(searchParams) as { history: History };
+    const { history } = (await this.request(searchParams)) as {
+      history: History;
+    };
     return history;
   }
 
   private async request(params: URLSearchParams): Promise<unknown> {
     const url = new URL(this.url.toString());
     params.forEach((value, key) => url.searchParams.set(key, value));
-    const { body } = await got.post(url, { responseType: 'json' });
+    const { body } = await got.post(url, { responseType: "json" });
     return body;
   }
-
 }
 
 export interface AddURLParams {
   /**
    * Name of the job, if empty the NZB filename is used.
    */
-  nzbname?: string
+  nzbname?: string;
   /**
    * Password to use when unpacking the job (new in 3.0.0+).
    */
-  password?: string
+  password?: string;
   /**
    * Category to be assigned, * means Default. List of available categories can be retrieved from get_cats.
    */
-  cat?: string
+  cat?: string;
   /**
    * Script to be assigned, Default will use the script assigned to the category. List of available scripts can be retrieved from get_scripts.
    */
-  script?: string
+  script?: string;
   /**
    * Priority to be assigned:
    * - -100 = Default Priority (of category)
@@ -169,7 +180,7 @@ export interface AddURLParams {
    * - 1 = High Priority
    * - 2 = Force
    */
-  priority?: number
+  priority?: number;
   /**
    * Post-processing options:
    * - -1 = Default (of category)
@@ -178,7 +189,5 @@ export interface AddURLParams {
    * - 2 = +Repair/Unpack
    * - 3 = +Repair/Unpack/Delete
    */
-  pp?: number
+  pp?: number;
 }
-
-
